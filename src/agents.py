6392a28c37c36a22156d4597131fd6565c6f0ca4
sreeja-moganty <sreeja.moganty@gmail.com>
2026-04-30 18:11:01 +0530

    Delete src directory

--- src/agents.py
+++
@@ -1,8 +0,0 @@
-def retriever_agent(query):
-    return retrieve(query, index, texts)
-
-def analyzer_agent(docs):
-    return "\n".join(docs[:3])
-
-def explainer_agent(query, context):
-    return generate_answer(query, context)
